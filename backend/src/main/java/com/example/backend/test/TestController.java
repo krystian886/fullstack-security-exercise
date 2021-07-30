8dccf2909b00b0ce14347ab7a6f59f11d08ec743
Krystian Turek <krystian.turek2@gmail.com>
2021-07-30 17:13:13 +0200

    preparing app template

---
+++ backend/src/main/java/com/example/backend/test/TestController.java
@@ -0,0 +1,17 @@
+package com.example.backend.test;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER')")
+    public String userAccess() {
+        return "user content";
+    }
+}
