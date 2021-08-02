4e85e96e9edcf3f18108bc0eceab0cf965332479
Krystian Turek <krystian.turek2@gmail.com>
2021-08-02 19:33:46 +0200

    frontend: added confirmation component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import LoginPage from './components/security/LoginPage'
 import RegistrationPage from './components/security/RegistrationPage';
 import ForgottenPassword from './components/security/ForgottenPassword';
+import Confirmation from './components/Confirmation';
 
 function App() {
   return (
     <>
     {/* <LoginPage/> */}
     {/* <RegistrationPage/> */}
-    <ForgottenPassword/>
+    {/* <ForgottenPassword/> */}
+    <Confirmation/>
     </>
   );
 }
