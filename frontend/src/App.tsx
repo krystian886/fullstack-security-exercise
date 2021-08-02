b68ef6cd224588ba22d2537cfefab9f7ae322208
Krystian Turek <krystian.turek2@gmail.com>
2021-08-02 18:50:05 +0200

    frontend: added registration form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import LoginPage from './components/LoginPage'
+import LoginPage from './components/security/LoginPage'
+import RegistrationPage from './components/security/RegistrationPage';
 
 function App() {
   return (
     <>
-    <LoginPage/>
+    {/* <LoginPage/> */}
+    <RegistrationPage/>
     </>
   );
 }
